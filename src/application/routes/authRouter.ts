e37f685a1b52a6c1eecbac368c3fe1d1f3c83408
ShunkovMikhail <gomarusanichiichi@yahoo.com>
2023-08-31 01:41:59 +0300

    some fixes

--- src/application/routes/authRouter.ts
+++ src/application/routes/authRouter.ts
@@ -1,14 +1,17 @@
 import { loginVdChain } from '../../inputValidation'
 import {
     APIErrorResult,
-    LoginInputModel, LoginSuccessViewModel,
+    LoginInputModel,
+    LoginSuccessViewModel,
+    MeViewModel,
     TypeOfRequestBody
 } from '../../types/models'
-import { Response, Router } from 'express'
+import { Request, Response, Router } from 'express'
 import { Result, validationResult } from 'express-validator'
 import { ErrorMapper } from '../../utils/errorMapper'
 import { usersService } from '../../domain/users-service'
 import { jwtService } from '../jwt-service'
+import { authMiddleware } from "../../middlewares/auth-middleware"
 
 export const authRouter = Router({})
 
@@ -19,11 +22,23 @@ authRouter.post('/login', loginVdChain, async (req: TypeOfRequestBody<LoginInput
     if (result.isEmpty()) {
         const user = await usersService.authenticate(req)
         if (user) {
-            res.status(204).json({accessToken: await jwtService.createToken(user)})
+            res.status(200).json({accessToken: await jwtService.createToken(user)})
         } else {
             res.sendStatus(401)
         }
     } else {
         res.status(400).json(await ErrorMapper(result))
     }
+})
+
+
+
+authRouter.get('/me', authMiddleware, async (req: Request, res: Response<MeViewModel | null>) => {
+
+    const me: MeViewModel = {
+        email: req.user!.email,
+        login: req.user!.login,
+        userId: req.user!.id
+    }
+        res.status(200).json(me)
 })
